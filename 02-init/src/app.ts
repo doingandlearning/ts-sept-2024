b6cd6db0693b55c9bc469cc22794d78c611b49df
Kevin Cunningham <kevin@kevincunningham.co.uk>
2024-09-26 15:36:40 +0100

    restores 02-init

---
+++ 02-init/src/app.ts
@@ -0,0 +1,11 @@
+console.log("I'm working!");
+
+const location = "Liverpool";
+
+function sayHello(location: string) {
+  console.log(`Hello from ${location}!`);
+}
+
+sayHello(location);
+
+export {};
