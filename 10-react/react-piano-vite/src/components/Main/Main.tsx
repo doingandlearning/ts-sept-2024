27febedc4c1d451159364f7e5f2a21fe80f3b6dc
Kevin Cunningham <kevin@kevincunningham.co.uk>
2024-09-26 12:37:37 +0100

    roll

--- 10-react/react-piano-vite/src/components/Main/Main.tsx
+++ 10-react/react-piano-vite/src/components/Main/Main.tsx
@@ -3,6 +3,6 @@ import { NoAudioMessage } from "../NoAudioMessage"
 import { useAudioContext } from "../AudioContextProvider"
 
 export const Main = () => {
-  const AudioContext = useAudioContext()
+  const AudioContext = useAudioContext() // useS => React Hook
   return (AudioContext) ? <Playground /> : <NoAudioMessage />
 }
---
+++ 11-errors/01-no-error-handling.ts
@@ -0,0 +1,13 @@
+function ask() {
+  return prompt("When is your birthday?") || "";
+}
+
+function parse(birthday) {
+  return new Date(birthday);
+}
+
+let date = parse(ask());
+console.log(`Date is ${date.toISOString()}`);
+
+// How might it break?
+// -
